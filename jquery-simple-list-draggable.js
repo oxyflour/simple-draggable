a26c765e5539c108b3fcabfa6033217c4cb33dcc
oxyflour <oxyflour@hotmail.com>
2015-11-25 21:42:05 +0800

    add start item class

--- jquery-simple-list-draggable.js
+++ jquery-simple-list-draggable.js
@@ -1,36 +1,46 @@
 +function($) {
 	var INDICATOR_CLASS = 'simple-draggable-indicator',
+		STARTITEM_CLASS = 'simple-draggable-start',
 		INDICATOR_STYLEID = 'simpleDraggableStyle',
 		INDICATOR_STYLE = [
 			'.' + INDICATOR_CLASS + ' {',
 				'position: absolute;',
 				'width: 100%;',
-				'height: 3px;',
-				'background: #333;',
-				'opacity: 0.6;',
-			'}'
+				'height: 2px;',
+				'background: #aaa;',
+				'opacity: 0.5;',
+			'}',
+			'.' + STARTITEM_CLASS + ' {',
+				'opacity: 0.5;',
+			'}',
 		].join('')
 
 	$.fn.simpleDraggable = function(callback) {
 		var list = $(this),
-			indicator = $('<div class="'+INDICATOR_CLASS+'"></div>')
+			indicator = $('<div class="'+INDICATOR_CLASS+'"></div>'),
+			startItem = null
 
 		if (!$('#'+INDICATOR_STYLEID).length)
 			$('<style id="'+INDICATOR_STYLEID+'">'+INDICATOR_STYLE+'</style>').prependTo('head')
 
 		function onMouseMove(e) {
-			var prev = list.children().filter(function() {
-				return !$(this).hasClass(INDICATOR_CLASS) && $(this).offset().top < e.pageY
-			}).last()
+			var next = null
+			list.children().get().some(function(elem) {
+				return (elem = $(elem)) && !elem.hasClass(INDICATOR_CLASS) &&
+					elem.offset().top + elem.height() > e.pageY && (next = elem)
+			})
 
-			if (prev.length) {
-				if (!prev.next().is(indicator))
-					indicator.remove().insertAfter(prev)
+			if (next) {
+				if (!next.prev().is(indicator))
+					indicator.remove().insertBefore(next)
 			}
 			else {
-				if (!list.children().first().is(indicator))
-					indicator.remove().prependTo(list)
+				if (!list.children().last().is(indicator))
+					indicator.remove().appendTo(list)
 			}
+
+			if (next && !startItem)
+				startItem = next.addClass(STARTITEM_CLASS)
 
 			e.preventDefault()
 		}
@@ -38,7 +48,11 @@
 		function onMouseUp(e) {
 			var index = list.children().index(indicator)
 
-			indicator.remove()
+			if (indicator.parent().length)
+				indicator.remove()
+			
+			if (startItem)
+				startItem.removeClass(STARTITEM_CLASS)
 
 			$(document).off('mousemove', onMouseMove).off('mouseup', onMouseUp)
 
